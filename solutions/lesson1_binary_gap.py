594fcc89ddae5d64081b5c136544d738611e3de0
ardenn <mc_alila@outlook.com>
2018-09-04 13:55:03 +0300

    Initial commit

---
+++ solutions/lesson1_binary_gap.py
@@ -0,0 +1,25 @@
+def solution(N):
+    start_count = False
+    max_gap = 0
+    current_gap = 0
+
+    # Generate binary rep on N
+    while N > 0:
+        if N%2 == 1:
+            if start_count:
+                max_gap = max(max_gap,current_gap)
+                current_gap = 0
+            else:
+                start_count = True
+        else:
+            if start_count:
+                current_gap += 1
+        N = int(N/2)
+    return max_gap
+
+
+if __name__ == '__main__':
+    print(solution(7))
+    print(solution(10417))
+    print(solution(457))
+    print(solution(107))
